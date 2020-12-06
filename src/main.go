8fb9cf2df9d9241820df1aea644d1538d7dd0ec5
Anatoly Brevnov <tbdapplications@gmail.com>
2020-12-06 13:11:48 -0500

    Swapped docker ids

--- src/main.go
+++ src/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bytes"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
 	"strings"
 	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
 var childServices = strings.Split(os.Getenv("CHILD_SERVICE"), ",")
@@ -33,7 +34,7 @@ func main() {
 		id := c.Param("id")
 		if len(childServices) > 0 {
 			body := generatePostBody(childServices, id)
-			resp, err := http.Post("http://coordinator.yac.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
+			resp, err := http.Post("http://coordinator-baseline.yac.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
 			if err == nil && resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 				c.Status(http.StatusOK)
 			} else if err != nil {
@@ -54,7 +55,7 @@ func main() {
 		id := c.Param("id")
 		if len(childServices) > 0 {
 			body := generateDeleteBody(childServices, id)
-			resp, err := http.Post("http://coordinator.yac.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
+			resp, err := http.Post("http://coordinator-baseline.yac.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
 			if err == nil && resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 				c.Status(http.StatusOK)
 			} else if err != nil {
