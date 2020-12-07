2c73c96671815c9e612992d32c16c1e3f68e6a22
Anatoly Brevnov <tbdapplications@gmail.com>
2020-12-07 18:21:00 -0500

    Updates

--- src/main.go
+++ src/main.go
@@ -32,7 +32,7 @@ func main() {
 			c.Status(http.StatusOK)
 		} else {
 			body := generatePostBody(childServices, id)
-			resp, err := http.Post("http://coordinator.yac-baseline.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
+			resp, err := http.Post("http://coordinator.yac.svc.cluster.local:8080/saga", "application/json", bytes.NewBuffer(body))
 			if err == nil && resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 				c.Status(http.StatusOK)
 			} else if err != nil {
